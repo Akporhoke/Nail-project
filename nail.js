2c34bcdadb0068ccf36ee3245ddc3ca5e3a61f8c
Akporhoke <akporhokea1@gmail.com>
2025-10-16 22:27:54 +0100

    update

--- nail.js
+++ nail.js
@@ -1,11 +1,3 @@
-const servicesElement=document.getElementById("services")
-const phoneNumberElement=document.getElementById("phoneNumber")
-const phoneNumberElement2=document.getElementById("phoneNumber2")
-const nameElement=document.getElementById("name")
-const emailElement=document.getElementById("email")
-const dateElement=document.getElementById("date")
-const noteElement=document.getElementById("notes")
-
 
 
 let logo = document.querySelector('.logo');
@@ -42,42 +34,3 @@ const displayMenu = event => {
 
  const submitButtonElement = document.getElementById("submit-btn")
 
-const submitForm = event => {
-    event.preventDefault()
-    const serviceContentElement = servicesElement.value
-    const phoneElement = phoneNumberElement.value
-    const phoneElement2 = phoneNumberElement2.value
-    const FullName=nameElement.value
-    const Email=emailElement.value
-    const date=dateElement.value
-    const note=noteElement.value
-    const phoneArr=[phoneElement.length]
-    const phoneArr2=[phoneElement2.length]
-    
-    
-    if(phoneArr<11 || phoneArr>12){
-            return alert('Check phone number')
-    }
-    else if(phoneArr2<11 || phoneArr2>12){
-            return alert('Check phone number')
-    }
-    
-    else if (phoneArr2==="") {
-            
-    }
-    else {}
-    
-
-    if ( serviceContentElement==="-Select a Service-"|| phoneElement===""  || phoneElement2===""|| FullName==="" || Email==="" || date==="" ) {
-            alert("-ENSURE ALL DETAILS OF YOUR APPOINTMENT ARE CORRECTLY FILLED OUT-")   
-    }
-    
-    else{
-        alert("YOU'LL BE CONTACTED IN 24HRS TO DISCUSS THE DETAILS OF YOUR APPOINTMENT")
-    }
-    
-    
-
-};
-
-submitButtonElement.addEventListener("click", submitForm);
