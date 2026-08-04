c19a2c15c8438597abe3e0a2c793aa5f70848002
Akporhoke <akporhokea1@gmail.com>
2026-08-04 16:14:24 +0100

    update appointment.js

--- appointment.js
+++ appointment.js
@@ -231,7 +231,10 @@ addServiceBtn.addEventListener("click", (e) => {
 });
 
 // ======= DISABLE PAST DATES =======
-
+const today = new Date();
+today.setDate(today.getDate() + 2); // Add 2 days
+const minDate = today.toISOString().split('T')[0];
+dateElement.min = minDate;
 
 // ======= RESTRICT TIME PICKER TO BUSINESS HOURS =======
 timeElement.min = "08:00";
